999e5fa3e83824b864c923cf355c65a5f4af437a
pcor4650 <pcor4650@gmail.com>
2022-11-02 19:36:20 +0900

    BFS_FloodFill 추가

--- 알고리즘개념/BFS.py
+++ 알고리즘개념/BFS.py
@@ -26,3 +26,32 @@ graph = [
 visited = [False] * 9
 
 bfs(graph, 1, visited)
+
+
+# FloodFill #
+from collections import deque
+def solution(n, m, image):
+    cnt = 0
+    visited = [[False]*m for _ in range(n)]
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
+    for i in range(n):
+        for j in range(m):
+            if not visited[i][j]:
+                q = deque()
+                q.append([i, j])
+                color = image[i][j]
+                while q:
+                    x, y = q.popleft()
+                    for k in range(4):
+                        nx = x + dx[i]
+                        ny = y + dy[i]
+                        if 0 <= nx < n and 0 <= ny < n:
+                            if not visited[nx][ny]:
+                                if image[nx][ny] == color:
+                                    visited[nx][ny] = True
+                                    q.append([nx, ny])
+                cnt += 1
+    return cnt
+    
+# 출처 : https://ygseo.tistory.com/300
