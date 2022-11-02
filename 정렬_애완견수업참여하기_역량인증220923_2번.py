4dd135c567bc220c28d9e664f520717522358cf7
pcor4650 <pcor4650@gmail.com>
2022-11-02 16:56:07 +0900

    정렬_애완견수업참여하기_역량인증220923_2번

---
+++ 정렬_애완견수업참여하기_역량인증220923_2번.py
@@ -0,0 +1,29 @@
+#2. 정답, 백준 1931과 유사
+
+import sys
+
+def Input_Data():
+	readl = sys.stdin.readline
+	N = int(readl())
+	info = [list(map(int,readl().split())) for _ in range(N)]
+	return N, info
+
+ans = 1
+N, info = Input_Data() # 입력 받는 부분
+
+# 여기서 부터 작성
+for i in range(N):
+	info[i][1] = info[i][0] + info[i][1] + 2
+
+info.sort(key = lambda x:x[0])
+info.sort(key = lambda x:x[1])
+
+end = info[0][1]
+
+for j in range(1,N):
+	if info[j][0] >= end:
+		ans += 1
+		end = info[j][1]
+		
+
+print(ans) # 출력 하는 부분
