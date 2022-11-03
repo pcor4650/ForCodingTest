70a9a79006a66ce34f32f128ffe0ad22cd6cc227
Chaohreum <pcor4650@gmail.com>
2022-11-03 14:34:11 +0900

    추가

--- 슬라이싱_바이러스검사_기출문제.py
+++ 슬라이싱_바이러스검사_기출문제.py
@@ -7,10 +7,15 @@ def InputData():
     Virus = list(map(int, readl().split()))
     return N, M, Data, Virus
 
+# 기초 바이러스로 변환
+global Virus
+Virus.sort()
+Virus = list(map(lambda x:X-[Virus[0], Virus])) 
+
 def find(start):
     global Virus
 
-    check_array = Data[start:start+M]
+    check_array = A[start:start+M]
     check_array.sort()
     check_array = list(map(lambda x:x-check_array[0], check_array))
 
@@ -24,10 +29,5 @@ def Solve():
 
 sol = 0
 N, M, Data, Virus = InputData()
-# 기초 바이러스로 변환
-global Virus
-Virus.sort()
-Virus = list(map(lambda x:X-[Virus[0], Virus])) 
-
 sol = Solve()
 print(sol)
