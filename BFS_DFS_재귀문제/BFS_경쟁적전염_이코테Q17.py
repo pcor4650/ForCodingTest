d55302c1b2b2dce1e366799f27c62bacfe4ed4c6
Chaohreum <pcor4650@gmail.com>
2022-11-08 22:38:11 +0900

    문제 및 풀이방법 추가

--- BFS_DFS_재귀문제/BFS_경쟁적전염_이코테Q17.py
+++ BFS_DFS_재귀문제/BFS_경쟁적전염_이코테Q17.py
@@ -41,30 +41,16 @@ while q:
 print(graph[target_x-1][target_y-1])   #출력할때도 주의!
 
 
-# 내가 짜본 코드
-# n, k = map(int, input().split())
+# 문제
+# 입력조건 : 
+# 첫째 줄에 자연수 N(1~200), K(1~1000)가 주어지며, 각 자연수는 공백으로 구분
+# 둘째 줄부터 N개의 줄에 걸쳐 시험관의 정보가 주어진다. 바이러스가 존재하지 않는 경우 0, 존재하면 각 바이러스 번호가 입력
+# N+2 번째 줄에는 S(1~10,000), X, Y가 주어지며 공백으로 구분
 
-# graph = []
-# for _ in range(n):
-#     graph.append(list(map(int, input().split())))
+# 3 3
+# 1 0 2   ->   1 1 2   ->   1 1 2        
+# 0 0 0        1 0 2        1 1 2
+# 3 0 0        3 3 0        3 3 2
+# 2 3 2
 
-# s, x, y = map(int, input().split())
-
-# dx = [-1, 1, 0, 0]
-# dy = [0, 0, -1, 0]
-
-# def sol(s, x, y):
-#     for _ in range(1, s+1):
-#         for i in range(n):
-#             for j in range(n):
-#                 if graph[i][j] != 0:
-#                     for k in range(4):
-#                         nx = i + dx[k]
-#                         ny = j + dy[k]
-#                         if nx >=0 and nx < n and ny >= 0 and ny < n:
-#                             if graph[nx][ny] == 0:
-#                                 graph[nx][ny] = graph[i][j]
-#                             elif graph[nx][ny] != 0 :
-#                                 graph[nx][ny] = min(graph[i][j], graph[nx][ny])
-
-# sol(s, x, y)
+# data = [(1, 0, 0, 0), (2, 0, 0, 2), (3, 0, 2, 0)]
