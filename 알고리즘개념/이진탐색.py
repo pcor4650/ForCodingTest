7f83c30a4ac44ff4a59cda8f6fa324764fed48a9
Chaohreum <pcor4650@gmail.com>
2022-11-10 22:54:48 +0900

    이진탐색 bisect 라이브러리 추가

--- 알고리즘개념/이진탐색.py
+++ 알고리즘개념/이진탐색.py
@@ -36,3 +36,30 @@ def binary_search(array, target, start, end):
         else :
             start = mid + 1
     return None
+
+# 파이썬에서는 이진 탐색을 쉽게 구현할 수 있도록 bisect 라이브러리를 제공한다.
+# bisect 라이브러리는 정렬된 배열에서 특정한 원소를 찾아야 할 때 매우 효과적으로 사용된다.
+
+# bisect_left(a, x) : 정렬된 순서를 유지하면서 리스트 a에 데이터 x를 삽입할 가장 왼쪽 인덱스를 찾는 메서드
+# bisect_right(a, x) : 정렬된 순서를 유지하도록 리스트 a에 데이터 x를 삽입할 가장 오른쪽 인덱스를 찾는 메서드
+
+# 예를 들어 정렬된 리스트 [1, 2, 4, 4, 8]이 있을 때, 새롭게 데이터 4를 삽입하려 한다고 가정하자. 
+# 이때 bisect_left(a, 4) = 2, bisect_right(a, 4) = 4 를 반환한다.
+# 1       2       4       4       8
+#            ↑bisect_left(a,4)
+
+# bisect_left()함수와 bisect_right()함수는 정렬된 리스트에서 값이 특정 범위에 속하는 원소의 개수를 구하고자 할 때 효과적으로 사용될 수 있다.
+
+from bisect import bisect_left, bisect_right
+
+# 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
+def count_by_range(a, left_value, right_value):
+    right_index = bisect_right(a, right_value)
+    left_index = bisect_left(a, left_value)
+    return right_index - left_index
+
+# 리스트 선언
+a = [1, 2, 3, 3, 3, 3, 4, 4, 8, 9]
+
+# 값이 4인 데이터 개수 출력
+print(count_by_range(a, 4, 4))
