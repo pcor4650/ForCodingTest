884e6a5000c25031d0032eba41c43eb280f8cd1c
pcor4650 <pcor4650@gmail.com>
2023-02-08 14:17:49 +0900

    Update 스택_건물옥상정원_기출문제.py

--- swedu기출문제/스택_건물옥상정원_기출문제.py
+++ swedu기출문제/스택_건물옥상정원_기출문제.py
@@ -1,17 +1,28 @@
-import sys
-read = sys.stdin.readline
-N = int(read())
-A = []
-ans = 0
+# 내 건물이 보이는 옥사의 수 계산
 
-for i in range(N):
-    x = int(input())
-    if not A:
-        A.append(x)
-    else:
-        while A and A[-1] <= x:
-            A.pop()
-        ans += len(A)
-        A.append(x)
-print(ans)
-    
+# stack = []	[5]		[5, 2]		[5]    [5, 4]		[5, 4]  [5,4,3]  		[5, 4] [5] []  [6]  		[6, 1]
+# cnt = 0			 0		   1  		 1        2	 		  4              		                    		  5
+
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+Heights = []
+for _ in range(N):
+	Heights.append(int(input()))
+
+stack = []
+cnt = 0
+
+for height in Heights:
+	if not stack:
+		stack.append(height)	
+	else:
+		while stack and stack[-1] <= height:
+			stack.pop()
+		cnt += len(stack)
+		stack.append(height)
+
+print(cnt)
+print(stack)
