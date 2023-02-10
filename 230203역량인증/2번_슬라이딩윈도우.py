1107cf676d77737b40744f7a65dc1482890f12aa
Chaohreum <pcor4650@gmail.com>
2023-02-11 00:34:38 +0900

    파일이름 및 경로 정리

--- 230203역량인증/2번_슬라이딩윈도우.py
+++ 230203역량인증/2번_슬라이딩윈도우.py
@@ -55,3 +55,27 @@ for start in range(N):
 # 		min_sum = min(min_sum, interval_sum)
 	
 print(min_sum)
+
+
+# s, e 가 있을 때? 인터벌의 합이 가장 작은 것은
+# 230203 역량인증 2번 아래와 같이 풀면 맞을 듯
+n, s, e = 5, 1, 2
+datas = [3, -1, -2, 3, 1]
+
+min_sum = 10000000
+prefix_sum = [0]
+summ_value = 0
+for dta in datas:
+    summ_value += dta
+    prefix_sum.append(summ_value)
+
+print("prefix_sum: ", prefix_sum)
+
+for i in range(s, e+1):
+    for j in range(1, n-i+1):
+        left = j
+        right = j + i -1
+        part_sum = prefix_sum[right] - prefix_sum[left-1]
+        min_sum = min(min_sum, part_sum)
+        print("left:", left, ", right:", right, ", part_sum:", part_sum, ", min_sum: ", min_sum)
+print(min_sum)
