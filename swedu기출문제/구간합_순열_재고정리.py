9c5228a1df3bcb55ad702317045c5602b780e358
Chaohreum <pcor4650@gmail.com>
2023-02-19 22:57:09 +0900

    230219

--- swedu기출문제/구간합_순열_재고정리.py
+++ swedu기출문제/구간합_순열_재고정리.py
@@ -3,12 +3,39 @@
 # 1 2 2 2 1 2 1
 # 1 : 3개, 2 : 4개
 
+# from itertools import permutations
+
+# N, M = map(int, input().split())
+# A = list(map(int, input().split()))
+# num = [0]*10
+# psum = [[0 for _ in range(N+1)] for _ in range(M+1)]
+
+# for i, a in enumerate(A):
+#     num[a] += 1
+#     for b in range(1, M+1):
+#         psum[b][i+1] = psum[b][i]
+#         if a == b:
+#             psum[b][i+1] += 1
+
+# total = 0
+# for P in permutations(range(1,M+1)):
+#     idx = 0
+#     cnt = 0
+#     for p in P:
+#         s,e = idx, idx+num[p]
+#         cnt += psum[p][e]-psum[p][s]
+#         idx = e
+#     total = max(total, cnt)
+# print(N-total)
+
+import sys
+input = sys.stdin.readline
 from itertools import permutations
 
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
 num = [0]*10
-psum = [[0 for _ in range(N+1)] for _ in range(M+1)]
+psum = [[0]*(N+1) for _ in range(M+1)]
 
 for i, a in enumerate(A):
     num[a] += 1
@@ -18,13 +45,15 @@ for i, a in enumerate(A):
             psum[b][i+1] += 1
 
 total = 0
-for P in permutations(range(1,M+1)):
+print("permutations(range(1, M+1)) : ", list(permutations(range(1, M+1))))
+for P in permutations(range(1, M+1)):
     idx = 0
     cnt = 0
     for p in P:
-        s,e = idx, idx+num[p]
+        print("p: ", p)
+        s, e = idx, idx+num[p]
         cnt += psum[p][e]-psum[p][s]
         idx = e
+        print("s: ", s, ", e: ", e, ", cnt: ", cnt)
     total = max(total, cnt)
 print(N-total)
-
--- swedu기출문제/샘플제품사진.py
+++ swedu기출문제/샘플제품사진.py
@@ -3,25 +3,36 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-items = []
-ids = []
+array = []
+id_set = set()
 for _ in range(N):
-    x, id = map(int, input().split())
-    items.append([x, id])
-    ids.append(id)
+    pos, id = map(int, input().split())
+    array.append([pos, id])
+    id_set.add(id)
 
-ids = list(set(ids))
-ids.sort()
-items.sort()
+new_id = dict()
+cnt_id = 0
 
-# print(ids)
-# print(items)
+for id in sorted(list(id_set)):
+    new_id[id] = cnt_id
+    cnt_id += 1
 
-check = [0]*len(ids)
+new_array = []
+for x, i in array:
+    new_array.append([x, new_id[i]])
+new_array.sort()
 
-ans = 2000000001
+check = [0]*cnt_id
 
-S = 0
-cnt = 0
-for E in range(N):
-    
+min_length = 2000000001
+s = 0
+
+for e in range(N):
+    check[new_array[e][1]] += 1
+    print("s: ", s, ", e: ", e, ", check: ", check )
+    while 0 not in check:
+        print("min_length: ", min_length, ", new_array[e][0] - new_array[s][0]: ", new_array[e][0] - new_array[s][0])
+        min_length = min(min_length, new_array[e][0] - new_array[s][0])
+        check[new_array[s][1]] -= 1
+        s += 1
+print(min_length)
